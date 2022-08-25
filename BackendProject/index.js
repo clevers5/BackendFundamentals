911010fca7a87471b3583147815787514d0a7d37
Clever <arnez.4292@gmail.com>
2022-08-25 00:31:04 -0400

    Config express y peticion

--- BackendProject/index.js
+++ BackendProject/index.js
@@ -1,1 +1,14 @@
-console.log("hola mundo");
+const express = require('express');
+
+const app = express();
+
+// index, root, home, página principal
+//request --> Solicitud
+//response --> Respuesta
+app.get('/',function(request, response){
+    console.log(request);
+    return response.send("<h1>Hola mundo</h1>");
+})
+
+app.listen(4080,()=>{
+    console.log('Server is running on port 4080');}); //Callback function
