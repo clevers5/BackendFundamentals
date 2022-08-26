94b7ebb66423b11acc456caa6b6edf4529cd4561
Clever <arnez.4292@gmail.com>
2022-08-26 00:42:47 -0400

    Data parsing

--- BackendProject/index.js
+++ BackendProject/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
-
+const multer = require('multer');
 const app = express();
+
+// Middleware --> Se ejecuta antes de la petición
+
+// Middleware --> para x-www-form-urlencoded
+app.use(express.urlencoded({
+    extended: true
+})) //use: es una función que se ejecuta en el medio de la petición
+
+// Middleware --> para .json
+app.use(express.json()) 
+
+// Middleware --> para form-data
+app.use(multer().none())
 
 // index, root, home, página principal
 //request --> Solicitud
@@ -11,9 +24,15 @@ app.get('/',function(request, response){
 })
 
 app.post("/saludar",function(request, response){
-    console.log(request.body);
-    return response.status(201).send("<h1>Hola</h1>");
+    const nombre = request.body.nombre;
+    return response.status(201).send(`<h1>Hola ${nombre}</h1>`);
 })
+
+//app.put()
+//app.delete()
 
 app.listen(4080,()=>{
     console.log('Server is running on port 4080');}); //Callback function
+
+    //NODEMON --> Para ejecutar el servidor en el puerto 4080 sin tener que reiniciar cuando realicemos algun cambio
+    // Comando --> "dev":"npx nodemon index.js" --> npm run dev
